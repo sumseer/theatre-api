e920d98cd73a4966b951472e846fa808c5400381
Max Savenya <savenya2013@gmail.com>
2025-01-02 03:07:07 +0200

    update user urls

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from .views import CreateUserView, ManageUserView
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_refresh"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
